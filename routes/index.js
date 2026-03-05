e47398921a903125e480a01ac63257b991d9b5f4
Kevin Guzman <kguzman@bdgsa.net>
2026-03-05 12:13:31 -0600

    eliminating coments

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,10 @@ const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
 
-// Ruta para la documentación
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
-// Tus rutas de contactos
+
 router.use('/', require('./contacts'));
 
 module.exports = router;
